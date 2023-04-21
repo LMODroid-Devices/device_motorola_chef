fd6a9ba995b64d10ec1cdb7b3a7ff4b00da2dd59
LMO GerritBot <gerrit@libremobileos.com>
2023-04-21 22:16:30 +0000

    Adapt to LMODroid by device-add script
    Change-Id: I3721553dbf6f366ae8425f2a6e194088fe16a89c

--- lmodroid_chef.mk
+++ lmodroid_chef.mk
@@ -16,7 +16,7 @@
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -54,7 +54,7 @@ TARGET_RECOVERY_FSTAB := device/motorola/sdm660-common/rootdir/etc/fstab.qcom
 
 # Device identifiers
 PRODUCT_DEVICE := chef
-PRODUCT_NAME := lineage_chef
+PRODUCT_NAME := lmodroid_chef
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola one power
 PRODUCT_MANUFACTURER := motorola
